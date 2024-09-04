497c046ac942fce3ccfa9c0490670a8c9cfae795
minthanhh <mint03sanzz@gmail.com>
2024-09-04 12:19:17 +0700

    feat: added readme

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf1e2241b93114f7aee311eb7f2ffca10322939")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6862b8d22122c31f545802e7b1776faa3404cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
